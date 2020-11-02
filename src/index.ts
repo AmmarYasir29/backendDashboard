b1fdc3cecea966eaad72b1809bbcd9c607e07973
Ammar Yasir <ammar29.ay@gmail.com>
2020-11-02 16:27:53 +0300

    done

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,12 @@ import {createConnection} from "typeorm";
 import * as express from "express";
 const app = express();
 import router from "./routes"
-import {Student} from "./entity/Students"
+const port = process.env.PORT || 3000;
 
 createConnection().then(async connection => {
 app.use(express.json());
 
 app.use("/",router)
 
-app.listen(3000, () => console.log("run on port 3000!"));
+app.listen(port, () => console.log(`run on port ${port}!`));
 }).catch(error => console.log(error));
